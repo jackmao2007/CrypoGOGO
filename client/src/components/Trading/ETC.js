6aede64310680e337c17d880178f6b9198470804
root <root@DESKTOP-1AD2ML4.localdomain>
2020-11-08 23:25:04 -0500

    merge stash

---
+++ client/src/components/Trading/ETC.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Navbar from "../Navbar/index";
+import Chart from "./Chart"
+
+class ETC extends Component {
+    state = {  }
+    render() { 
+        return ( 
+                <div>
+                </div>
+                );
+    }
+}
+ 
+export default ETC;
