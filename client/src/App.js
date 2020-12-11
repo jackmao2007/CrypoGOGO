c41774f92358533d753eec9d339ee96f5e06a8f6
Andy8647 <andy8647lgc@gmail.com>
2020-12-11 15:41:55 -0500

    footer modified

--- client/src/App.js
+++ client/src/App.js
@@ -61,7 +61,7 @@ class App extends Component {
         <Route exact path='/profile' render={ props => 
                   (!this.state.loginStatus ? <Login {...props} app={this} /> : <Profile/>)}/>                
       </Switch>
-      <footer className="App-footer"> ©2020 Created by Team45 at <a ref="https://github.com/csc309-fall-2020/team45"> Github </a> </footer>
+      <footer className="App-footer"> ©2020 Created by Team45 at <a href="https://github.com/csc309-fall-2020/team45" target="_blank"> Github </a> </footer>
 
       </BrowserRouter>
      );
